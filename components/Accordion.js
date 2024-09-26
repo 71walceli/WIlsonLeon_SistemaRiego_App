f28e1f56b9df4caddd6f5a6518b40778b6854753
wilsonleon <antonini2903@hotmail.com>
2024-09-26 08:26:43 -0500

    Quitado código de animaciones hasta hacerlo funcional

--- components/Accordion.js
+++ components/Accordion.js
@@ -1,45 +1,12 @@
+// Original from https://snack.expo.dev/@bgpiggin/accordion-list
 import React, { useState, useRef } from 'react';
-import {
-  View,
-  Text,
-  TouchableWithoutFeedback,
-  StyleSheet,
-  Animated,
-  Easing,
-} from 'react-native';
+import { View, Text, TouchableWithoutFeedback, StyleSheet, } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 
 const AccordionListItem = ({ title, children }) => {
   const [open, setOpen] = useState(true);
-  //const animatedController = useRef(new Animated.Value(0)).current;
-  //const [bodySectionHeight, setBodySectionHeight] = useState();
-
-  /* const bodyHeight = animatedController.interpolate({
-    inputRange: [0, 1],
-    outputRange: [0, bodySectionHeight],
-  });
-
-  const arrowAngle = animatedController.interpolate({
-    inputRange: [0, 1],
-    outputRange: ['0rad', `${Math.PI}rad`],
-  }); */
-
-  const toggleListItem = () => {
-    /* if (open) {
-      Animated.timing(animatedController, {
-        duration: 300,
-        toValue: 0,
-        easing: Easing.bezier(0.4, 0.0, 0.2, 1),
-      }).start();
-    } else {
-      Animated.timing(animatedController, {
-        duration: 300,
-        toValue: 1,
-        easing: Easing.bezier(0.4, 0.0, 0.2, 1),
-      }).start();
-    } */
-    setOpen(!open);
-  };
+  
+  const toggleListItem = () => setOpen(!open);
 
   return (
     <View style={styles.container}>
@@ -47,15 +14,11 @@ const AccordionListItem = ({ title, children }) => {
         <View style={styles.titleContainer}>
           <Text>{title}</Text>
           <MaterialIcons name="keyboard-arrow-down" size={20} color="black" />
-          {/* <Animated.View style={{ transform: [{ rotateZ: arrowAngle }] }}>
-          </Animated.View> */}
         </View>
       </TouchableWithoutFeedback>
       <View style={{...styles.bodyContainer, display: open ? "flex" : "none", }}>
         {children}
       </View>
-      {/* <Animated.View style={[styles.bodyBackground, { height: bodyHeight }]}>
-      </Animated.View> */}
     </View>
   );
 };
@@ -88,7 +51,6 @@ const styles = StyleSheet.create({
   bodyContainer: {
     padding: '1rem',
     paddingLeft: '1.5rem',
-    //position: 'absolute',
     bottom: 0,
   },
 });
