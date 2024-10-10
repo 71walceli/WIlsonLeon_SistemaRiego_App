cb7ac6805eb872e9504fb47750035de688dced3e
wilsonleon <antonini44354@gmail.com>
2024-10-10 11:57:44 -0500

    Últimos cambios

--- screens/Usuarios.js
+++ screens/Usuarios.js
@@ -1,18 +1,158 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { 
-  View, FlatList, StyleSheet, Text, TouchableOpacity,Pressable, ActivityIndicator, ScrollView,
-  TextInput, Modal, Alert, RefreshControl, TouchableWithoutFeedback, Keyboard, Image, Dimensions
+  View, FlatList, Text, TouchableOpacity,Pressable, ActivityIndicator, ScrollView,
+  TextInput, Alert, ToastAndroid
 } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Picker } from '@react-native-picker/picker';
 
 import { globalStyles } from './estilos/estilologin.js'; 
 import { styles } from '../styles/forms.js';
+import { validateCedula, validateEmail, validatePassword } from '../validaciones/validarinputs.js';
 
+
+const Formulario = ({ formState, formMode, handleInputChange, handleSubmit, saving, handleClose, handleDelete }) => {
+  useEffect(() => () => console.log("rerender form"))
+
+  const formErrors = {
+    nombre: useMemo(
+      () => [null, undefined, ""].includes(formState.nombre) ? "Requerido" : null,
+      [formState.nombre]
+    ),
+    apellido: useMemo(
+      () => [null, undefined, ""].includes(formState.apellido) ? "Requerido" : null,
+      [formState.apellido]
+    ),
+    email: useMemo(
+      () => ![null, undefined, ""].includes(formState.email) ? validateEmail(formState.email) : "Requerido",
+      [formState.email]
+    ),
+    cedula: useMemo(
+      () => ![null, undefined, ""].includes(formState.cedula) ? validateCedula(formState.cedula) : "Requerido",
+      [formState.cedula]
+    ),
+    password: useMemo(
+      () => ![null, undefined, ""].includes(formState.password) 
+        ? validatePassword(formState.password) 
+        : formMode === "edit" ? null : "Requerido"
+      ,
+      [formState.password]
+    ),
+  }
+
+  return (
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <View style={styles.formContainer}>
+        <Text style={styles.formTitle}>{formMode === 'edit' ? 'Editar' : 'Agregar'} Usuario</Text>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Nombre del Usuario</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Nombre del usuario"
+            value={formState.nombre || ''}
+            onChangeText={(text) => handleInputChange('nombre', text)}
+          />
+          {formErrors.nombre 
+            && <Text style={globalStyles.error}>{formErrors.nombre}</Text>
+          }
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Apellido del Usuario</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Apellido del usuario"
+            value={formState.apellido || ''}
+            onChangeText={(text) => handleInputChange('apellido', text)}
+          />
+          {formErrors.apellido 
+            && <Text style={globalStyles.error}>{formErrors.apellido}</Text>
+          }
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Cédula del usuario</Text>
+          <TextInput
+            keyboardType='numeric'
+            style={styles.input}
+            placeholder="Cédula del usuario"
+            value={formState.cedula || ''}
+            onChangeText={(text) => handleInputChange('cedula', text)}
+          />
+          {formErrors.cedula 
+            && <Text style={globalStyles.error}>{formErrors.correo}</Text>
+          }
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Rol del Usuario</Text>
+          <Picker
+            selectedValue={formState.id_rol}
+            onValueChange={(itemValue) => handleInputChange("id_rol", itemValue)}
+          >
+            <Picker.Item label="Jornalero" value={1} />
+            <Picker.Item label="Administrador" value={2} />
+          </Picker>
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Dirección de e-mail del Usuario</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Dirección de e-mail del usuario"
+            value={formState.email || ''}
+            onChangeText={(text) => handleInputChange('email', text)}
+          />
+          {formErrors.correo 
+            && <Text style={globalStyles.error}>{formErrors.correo}</Text>
+          }
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Contraseña del Usuario</Text>
+          <TextInput
+            secureTextEntry={true}
+            style={styles.input}
+            keyboardType='pass'
+            placeholder={formMode === "edit" ? "Contraseña actual del usuario" : "Contraseña del usuario"}
+            value={formState.password || ''}
+            onChangeText={(text) => handleInputChange('password', text)}
+          />
+          {formErrors.password
+            && <Text style={globalStyles.error}>{formErrors.password}</Text>
+          }
+        </View>
+        
+        <Pressable style={styles.saveButton} onPress={() => {
+          if (Object.values(formErrors).find(v => v !== null)) {
+            Alert.alert('Todos los campos deben estar validados');
+            return;
+          }
+          handleSubmit();
+        }}>
+          {saving
+            ?<ActivityIndicator color="#fff" />
+            :<Text style={globalStyles.text}>{formMode === 'edit' ? 'Guardar' : 'Ingresar'}</Text>
+          }
+        </Pressable>
+        <Pressable style={styles.cancelButton} onPress={handleClose}>
+          <Text style={globalStyles.text}>Cancelar</Text>
+        </Pressable>
+        {/* {formMode === "edit" 
+          &&<Pressable style={styles.cancelButton} onPress={handleDelete}>
+            <Text style={globalStyles.text}>Eliminar Usuarior</Text>
+          </Pressable>
+        } */}
+      </View>
+    </ScrollView>
+  );
+};
 
 const UsuariosScreen = () => {
   const [userData, setUserData] = useState({});
   const [formState, setFormState] = useState({}); // Estado para almacenar el estado del formulario
-  const [formMode, setFormMode] = useState('edit'); // Estado para controlar el modo del formulario (edit o add)
+  const [formMode, setFormMode] = useState('add'); // Estado para controlar el modo del formulario (edit o add)
   const [showForm, setShowForm] = useState(false)
 
   const [loading, setLoading] = useState(false);
@@ -51,9 +191,7 @@ const UsuariosScreen = () => {
     }
   };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  useEffect(() => { fetchData(); }, []);
 
   const handleRefresh = () => {
     // Función para volver a cargar los datos
@@ -87,75 +225,52 @@ const UsuariosScreen = () => {
     }
   };
 
+  const handleSave = async () => {
+    if (saving) return;
+    try {
+      setSaving(true);
+      const response = await fetch(`${process.env.EXPO_PUBLIC_API_BASE_URL}/auth.php?op=${
+        formMode === "edit"
+          ?"EditarUsuario"
+          :"registroemple"
+      }`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formState),
+      });
+
+      if (!response.ok) {
+        let responseData = {};
+        responseData = await response.json();  
+        console.error(responseData.message); 
+        throw new Error(`HTTP error ${response.status}`); 
+      }
+      ToastAndroid.showWithGravity('Parámetro editado con éxito', 
+        ToastAndroid.SHORT,
+        ToastAndroid.CENTER,
+      );
+      handleRefresh(); // Actualizar la lista después de editar el parámetro
+      setShowForm(false); // Ocultar el formulario después de editar el parámetro
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      console.error(formState)
+      // Manejo del error
+    } finally {
+      setSaving(false);
+    }
+  };
+
   // Función para manejar el cambio en los campos del formulario
   const handleInputChange = (fieldName, value) => {
     setFormState({ ...formState, [fieldName]: value });
   };
 
   const handleSubmit = () => {
-    const numberRegex = /^[0-9]+(\.[0-9]*)?$/;
-    
-    if (formMode === 'edit') {
-       // Validación de valor_maximo
-     if (formMode.valor_maximo === null && formMode.valor_maximo === undefined &&
-      formMode.valor_maximo === '' && !numberRegex.test(formMode.valor_maximo)) {
-        Alert.alert('No puede ingresar letras o símbolos, solo se acepta el punto y números');
-        return;
-      }
-      // Validación de valor_minimo
-      if (formMode.valor_minimo === null && formMode.valor_minimo === undefined &&
-          formMode.valor_minimo === '' && !numberRegex.test(formMode.valor_minimo)) {
-        Alert.alert('No puede ingresar letras o símbolos, solo se acepta el punto y números');
-        return;
-      }
-      handleEditado(
-        formState.nombre_parametro,
-        formState.descripcion,
-        formState.valor_minimo,
-        formState.valor_maximo,
-        formState.unidad_medida,
-        formState.estado_parametro,
-        formState.id, // Envía el id del parámetro al backend
-      );
-    } else if (formMode === 'add') {
-       // Validación de valor_maximo
-      if (formMode.valor_maximo === null && formMode.valor_maximo === undefined &&
-        formMode.valor_maximo === '' && !numberRegex.test(formMode.valor_maximo)) {
-          Alert.alert('No puede ingresar letras o símbolos, solo se acepta el punto y números');
-          return;
-        }
-        console.log(formMode.valor_minimo);
-        // Validación de valor_minimo
-        if (formMode.valor_minimo === null && formMode.valor_minimo === undefined &&
-            formMode.valor_minimo === '' && !numberRegex.test(formMode.valor_minimo)) {
-          Alert.alert('No puede ingresar letras o símbolos, solo se acepta el punto y números');
-          return;
-        }
-
-      if(formMode.valor_maximo ==='0.00' && formMode.valor_minimo ==='0.00' && formMode.valor_minimo ==='0' && formMode.valor_maximo ==='0' && formMode.valor_minimo ==='0.0' && formMode.valor_maximo ==='0.0'){
-        Alert.alert('No puede ingresar valores en cero dejelos vacio si no los usa.');
-        return;
-      } 
-      handleAgregado(
-        formState.nombre_parametro,
-        formState.descripcion,
-        formState.valor_minimo,
-        formState.valor_maximo,
-        formState.unidad_medida,
-        formState.estado_parametro,
-      );
-      // Limpia los campos del formulario después de agregar
-      setFormState({});
-    }
+    handleSave();
     setShowForm(false); // Oculta el formulario después de guardar o cancelar
-  };
-
-  const toggleFormMode = () => {
-    if (formMode === 'edit') {
-      setFormMode('add');
-      setFormState({}); // Limpia los campos del formulario al cambiar a modo 'add'
-    }
-    setShowForm(true); // Mostrar el formulario al cambiar el modo a 'add'
   };
 
   const renderItem = ({ item }) => (
@@ -175,106 +290,29 @@ const UsuariosScreen = () => {
 
   const handleItemPress = (item) => {
     // Aquí podrías navegar a una pantalla de detalle o editar según el ítem seleccionado
+    setFormMode("edit");
+    setShowForm(true)
+    setFormState({ ...item, email: item.correo, id_rol: Number(item.id_rol) });
     console.log('Item seleccionado:', item);
-  };
-
-  const Formulario = () => {
-    useEffect(() => () => console.log("rerender form"))
-
-    return (
-      <ScrollView contentContainerStyle={styles.scrollContainer}>
-        <View style={styles.formContainer}>
-          <Text style={styles.formTitle}>{formMode === 'edit' ? 'Editar' : 'Agregar'} Usuario</Text>
-
-          <View style={styles.inputContainer}>
-            <Text style={styles.label}>Nombre del Usuario</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Nombre del usuario"
-              value={formState.numbre || ''}
-              onChangeText={(text) => handleInputChange('numbre', text)}
-            />
-          </View>
-
-          <View style={styles.inputContainer}>
-            <Text style={styles.label}>Apellido del Usuario</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Apellido del usuario"
-              value={formState.apellido || ''}
-              onChangeText={(text) => handleInputChange('apellido', text)}
-            />
-          </View>
-
-          <View style={styles.inputContainer}>
-            <Text style={styles.label}>Cédula del usuario</Text>
-            <TextInput
-              keyboardType='numeric'
-              style={styles.input}
-              placeholder="Cédula del usuario"
-              value={formState.cedula || ''}
-              onChangeText={(text) => handleInputChange('cedula', text)}
-            />
-          </View>
-
-          <View style={styles.inputContainer}>
-            <Text style={styles.label}>Rol del Usuario</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Rol del usuario"
-              value={formState.id_rol || ''}
-              onChangeText={(text) => handleInputChange('id_rol', text)}
-            />
-          </View>
-
-          <View style={styles.inputContainer}>
-            <Text style={styles.label}>Dirección del Usuario</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Dirección del usuario"
-              value={formState.direccion || ''}
-              onChangeText={(text) => handleInputChange('direccion', text)}
-            />
-          </View>
-
-          <View style={styles.inputContainer}>
-            <Text style={styles.label}>Dirección de e-mail del Usuario</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Dirección de e-mail del usuario"
-              value={formState.correo || ''}
-              onChangeText={(text) => handleInputChange('correo', text)}
-            />
-          </View>
-
-          <View style={styles.inputContainer}>
-            <Text style={styles.label}>Contraseña del Usuario</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Contraseña del usuario"
-              value={formState.conrasena || ''}
-              onChangeText={(text) => handleInputChange('conrasena', text)}
-            />
-          </View>
-          
-          <Pressable style={styles.saveButton} onPress={handleSubmit}>
-            {saving
-              ?<ActivityIndicator color="#fff" />
-              :<Text style={globalStyles.text}>{formMode === 'edit' ? 'Guardar' : 'Ingresar parámetros'}</Text>
-            }
-          </Pressable>
-          <Pressable style={styles.cancelButton} onPress={() => setShowForm(false)}>
-            <Text style={globalStyles.text}>Cancelar</Text>
-          </Pressable>
-        </View>
-      </ScrollView>
-    );
   };
 
   return (
     <View style={styles.container}>
       {showForm 
-        ?<Formulario />
+        ?<Formulario 
+          formState={formState} 
+          formMode={formMode} 
+          handleInputChange={handleInputChange} 
+          handleSubmit={handleSubmit} 
+          handleClose={() => {
+            setShowForm(false);
+            setFormState({});
+          }}
+          handleDelete={() => {
+            // TODO
+          }}
+          saving={saving}
+        />
         :<>
           <FlatList
             data={userData}
@@ -287,7 +325,11 @@ const UsuariosScreen = () => {
               :<Text style={globalStyles.text}><Icon name="sync-alt" size={20} color="#FFFF" /> Actualizar</Text>
             }
           </Pressable>
-          <Pressable style={styles.button} onPress={() => setShowForm(true)}>
+          <Pressable style={styles.button} onPress={() => {
+            setShowForm(true);
+            setFormMode("add");
+            setFormState({})
+          }}>
             {saving
               ?<ActivityIndicator color="#fff" />
               :<Text style={globalStyles.text}><Icon name="add" size={20} color="#FFFF" /> Ingresar parámetros</Text>
